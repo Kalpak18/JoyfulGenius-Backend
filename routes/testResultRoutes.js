0cb878e15425730f1c7e90e50867fa7c59e0f2ed
Kalpak18 <bhoirkalpak916@gmail.cpm>
2025-08-13 01:39:10 +0530

    Update Backend for upload material

--- routes/testResultRoutes.js
+++ routes/testResultRoutes.js
@@ -19,14 +19,14 @@
 // export default router;
 import express from 'express';
 import { verifyUser, verifyAdmin } from '../middleware/auth.js';
-import { addManualTestByUser, addManualTestByAdmin, getUserTestResults, saveTheResult,deleteTestResult,updateTestResult } from '../controllers/testResultController.js';
+import { addManualTestByUser, addManualTestByAdmin, getUserResults, saveTheResult,deleteTestResult,updateTestResult } from '../controllers/testResultController.js';
 
 const router = express.Router();
 router.post("/save", verifyUser, saveTheResult);
 
 router.post('/add-manual', verifyUser, addManualTestByUser); // User adds own test
 router.post('/admin-add-manual', verifyAdmin, addManualTestByAdmin); // Admin adds test for any user
-router.get("/my", verifyUser, getUserTestResults);// Existing route to get user's test results
+router.get("/my", verifyUser, getUserResults);// Existing route to get user's test results
 router.put("/:id", verifyUser, updateTestResult); // ✅ Edit
 router.delete("/:id", verifyUser, deleteTestResult); // ✅ Delete
 
